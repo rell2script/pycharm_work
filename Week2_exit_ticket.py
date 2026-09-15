1caa99ce6ac32d3cc69a534ff0049d4663b7d587
rell2script <jarelln1213@gmail.com>
2026-09-15 15:12:06 -0400

    commit

---
+++ Week2_exit_ticket.py
@@ -0,0 +1,14 @@
+movie_name,genre,release_year,rating= "Obsession","Horror",2026,8.7
+favorite_actor,favorite_character="Inde","Nikkie"
+RATING=10
+#removed print statement from lines 5 and 6 for class
+print("My movie")
+print(f"Movie Name: {movie_name}. \n Genre: {genre} \n Release Year: {release_year} \n Rating: {rating} \n Favorite Actor: {favorite_actor} \n Favorite Character: {favorite_character}")
+#removed print statement from line 10 for class
+students=21
+students=students/3
+print(students)
+
+temp=86
+tempc=temp-32*5%9
+print(f" it is {temp} fahrenheit and {tempc} celsius")
