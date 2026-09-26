bbacadc0673e5ed32e6d4d2e937d865122ecfca7
rell2script <jarelln1213@gmail.com>
2026-09-25 22:59:07 -0400

    commit

---
+++ Project1_jarell.py
@@ -0,0 +1,68 @@
+
+# Shopper 1
+
+shopper_name = "Jarell Noble"
+
+grocery_1_price = 1.32
+grocery_2_price = 6.35
+grocery_3_price = 2.75
+grocery_4_price = 4.50
+grocery_5_price = 5.25
+
+grocery_1_quantity = 6
+grocery_2_quantity = 2
+grocery_3_quantity = 3
+grocery_4_quantity = 1
+grocery_5_quantity = 2
+
+total_bill = (grocery_1_price * grocery_1_quantity
+              + grocery_2_price * grocery_2_quantity
+              + grocery_3_price * grocery_3_quantity
+              + grocery_4_price * grocery_4_quantity
+              + grocery_5_price * grocery_5_quantity)
+
+total_items = (grocery_1_quantity
+    + grocery_2_quantity
+    + grocery_3_quantity
+    + grocery_4_quantity
+    + grocery_5_quantity)
+
+average_price = round(total_bill / total_items,2)
+
+print(f"{shopper_name.title()}: Total bill ${total_bill}, average price per item ${average_price}")
+
+
+# Shopper 2
+
+shopper_name = "Cindy James"
+
+grocery_1_price = 8.50
+grocery_2_price = 1.75
+grocery_3_price = 4.53
+grocery_4_price = 3.50
+grocery_5_price = 6.25
+
+grocery_1_quantity = 3
+grocery_2_quantity = 5
+grocery_3_quantity = 2
+grocery_4_quantity = 4
+grocery_5_quantity = 1
+
+total_bill = ( grocery_1_price * grocery_1_quantity
+    + grocery_2_price * grocery_2_quantity
+    + grocery_3_price * grocery_3_quantity
+    + grocery_4_price * grocery_4_quantity
+    + grocery_5_price * grocery_5_quantity
+)
+
+total_items = (grocery_1_quantity
+    + grocery_2_quantity
+    + grocery_3_quantity
+    + grocery_4_quantity
+    + grocery_5_quantity
+)
+
+average_price = round(total_bill / total_items,2)
+
+print(f"{shopper_name.title()}: Total bill ${total_bill}, average price per item ${average_price}")
+
---
+++ class temp.py
@@ -0,0 +1,3 @@
+numbers = [10, 20, 30, 40, 50, 60]
+for index in range(len(numbers)):
+    print(numbers[index])
